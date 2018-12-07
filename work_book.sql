c7dcfe15d49ff49b6bc09cebcc454adc171cf07b
jason6688 <284469828@qq.com>
2018-12-07 17:19:42 +0800

    路由

---
+++ work_book.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.5.53 : Database - work_book
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`work_book` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `work_book`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(30) DEFAULT NULL,
+  `user_pwd` char(32) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `add_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `work_book` */
+
+DROP TABLE IF EXISTS `work_book`;
+
+CREATE TABLE `work_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `row_record` varchar(255) NOT NULL,
+  `author` varchar(30) NOT NULL,
+  `Ymd` date NOT NULL,
+  `is_use` tinyint(1) NOT NULL DEFAULT '1' COMMENT '逻辑删除，默认1启用，0禁用',
+  `add_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `R_A_Y` (`row_record`,`author`,`Ymd`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
